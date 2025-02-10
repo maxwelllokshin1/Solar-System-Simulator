617f85c8db18d2f5e9d15db3e1235a90bbd3b5c9
Maxwell Lokshin <139578323+maxwelllokshin1@users.noreply.github.com>
2025-02-10 14:44:21 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -78,36 +78,34 @@ def main():
     desc_checkbox = CHECKBOX(checkboxX,checkbox_size,"Toggle Planet Descriptions")
     sliderList_checkbox = CHECKBOX(checkboxX,checkbox_size,"Toggle sliders")
 
-    # Create Sun and Planets
-    # sun = SUN(WIDTH // 2, HEIGHT // 2, SUN_MASS, sun_pic)
-    # Mercury = PLANET(mercury_distance, HEIGHT // 2, 0, mercury_velocity, MERCURY_MASS, "Mercury")
-    # Venus = PLANET(venus_distance, HEIGHT // 2, 0, venus_velocity, VENUS_MASS, "Venus")
-    # Earth = PLANET(earth_distance, HEIGHT // 2, 0, earth_velocity, EARTH_MASS, "Earth")
-    # Mars = PLANET(mars_distance, HEIGHT // 2, 0, mars_velocity, MARS_MASS, "Mars")
-    # Jupiter = PLANET(jupiter_distance, HEIGHT // 2, 0, jupiter_velocity, JUPITER_MASS, "Jupiter")
-    # Saturn = PLANET(saturn_distance, HEIGHT // 2, 0, saturn_velocity, SATURN_MASS, "Saturn")
-    # Uranus = PLANET(uranus_distance, HEIGHT // 2, 0, uranus_velocity, URANUS_MASS, "Uranus")
-    # Neptune = PLANET(neptune_distance, HEIGHT // 2, 0, neptune_velocity, NEPTUNE_MASS, "Neptune")
-
      # Create Sun and Planets
     sun = SUN(WIDTH // 2, HEIGHT // 2, 1.989, 4, "Sun")#, Sun_info)
-    Mercury = PLANET(mercury_distance, mercury_velocity, 3.30114, 3, "Mercury")#, Mercury_info)
-    Venus = PLANET(venus_distance, venus_velocity, 4.86747, 3, "Venus")#, Venus_info)
-    Earth = PLANET(earth_distance, earth_velocity, 5.97237 , 3, "Earth")#, Earth_info)
-    Mars = PLANET(mars_distance, mars_velocity, 6.41712, 3, "Mars")#, Mars_info)
-    Jupiter = PLANET(jupiter_distance, jupiter_velocity, 1.89819 , 3, "Jupiter")#, Jupiter_info)
-    Saturn = PLANET(saturn_distance, saturn_velocity, 5.68336 , 4, "Saturn")#, Saturn_info)
-    Uranus = PLANET(uranus_distance, uranus_velocity, 8.68127 , 4,"Uranus")#, Uranus_info)
-    Neptune = PLANET(neptune_distance, neptune_velocity, 1.02413, 4, "Neptune")#, Neptune_info)
+    Mercury = PLANET("Mercury")#, Mercury_info)
+    Venus = PLANET("Venus")#, Venus_info)
+    Earth = PLANET("Earth")#, Earth_info)
+    Mars = PLANET("Mars")#, Mars_info)
+    Jupiter = PLANET("Jupiter")#, Jupiter_info)
+    Saturn = PLANET("Saturn")#, Saturn_info)
+    Uranus = PLANET("Uranus")#, Uranus_info)
+    Neptune = PLANET("Neptune")#, Neptune_info)
 
     # slider (pos, name, multiplier)
-    sunMass = SLIDER(sliderWidth / 10, "SUN MASS", int(sun.mass), 1000)
+    sunMass = SLIDER(sliderWidth / 2, "SUN MASS", int(sun.mass), int(sun.mass) * SCALE)
     zoom = SLIDER( sliderWidth, "ZOOM IN/OUT", int(zoom_level), 10)
     speedUp = SLIDER(sliderWidth/5, "SPEED UP/ SLOW DOWN", int(fps_multiplier), 1000)
 
     objects = [Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune]# Add all planets to array
     allTogglable = [desc_checkbox, sliderList_checkbox]  #checkbox array
     allSliders = [sunMass, zoom, speedUp] # slider array
+
+    displacement = math.sqrt((Mercury.x - sun.x)**2 + (Mercury.y - sun.y) ** 2) # Calculates distance from the sun
+    print(displacement)
+    force = (GRAVCONST * sun.mass) / ((displacement*SCALE) ** 2) # Force Equation from PHYSICS
+    print(force)
+
+    acceleration = force/(1e4)
+
+    print(acceleration)
 
     # Run program
     while running:
@@ -170,6 +168,7 @@ def main():
                             if sliders.toggleSlider:
                                 sliders.pos = max(0, min(sliderWidth, mouse_pos[0] - sliderX))
                                 deltaSlider = max(1, (sliders.pos / sliderWidth) * sliders.deltaAmnt)
+
                                 sliders.changingVal = int(deltaSlider)
                                 match sliders.name:
                                     case "SUN MASS":
@@ -205,15 +204,9 @@ def main():
                         camera_follow = False
                         selected_planet = None
 
-
-            #TODO
             if camera_follow and selected_planet and not desc_checkbox.checked:
-                move_camera_x = selected_planet.x - SCREEN_WIDTH//2 - selected_planet.rad
-                move_camera_y = selected_planet.y - SCREEN_HEIGHT//2 - selected_planet.rad
-
-
-
-
+                move_camera_x = selected_planet.cameraX - SCREEN_WIDTH//2 - (selected_planet.rad//2)
+                move_camera_y = selected_planet.cameraY - SCREEN_HEIGHT//2 - (selected_planet.rad//2)
 
 
             win.blit(scaled_bg, (-bgPositioning[0] , -bgPositioning[1]))
@@ -222,14 +215,17 @@ def main():
                 pygame.draw.line(win, WHITE, (selected_planet.cameraX - move_camera_x, selected_planet.cameraY - move_camera_y), mouse_pos, 2)
 
 
+            # print(Mercury.cameraX - move_camera_x, Mercury.cameraY - move_camera_y)
             for obj in objects:
-                obj.draw(zoom_level, move_camera_x, move_camera_y)
-                # obj.move(sun)
+                obj.cameraX = (obj.x - WIDTH//2) * (zoom_level/10) + WIDTH // 2 
+                obj.cameraY = (obj.y - HEIGHT//2) * (zoom_level/10) + HEIGHT // 2
+                if(0<= obj.cameraX - move_camera_x <= SCREEN_WIDTH and 0 <= obj.cameraY - move_camera_y <= SCREEN_HEIGHT):
+                    obj.draw(zoom_level, move_camera_x, move_camera_y)
+                obj.move(sun)
 
-                off_screen = False # obj.x < 0 or obj.x > WIDTH or obj.y < 0 or obj.y > HEIGHT
                 collided_sun = math.sqrt((obj.x - sun.x)**2 + (obj.y - sun.y)**2) <= SUN_RADIUS*(zoom_level/10)
                 
-                if off_screen or collided_sun:
+                if collided_sun:
                     objects.remove(obj)
 
             sun.draw(zoom_level, move_camera_x, move_camera_y)
